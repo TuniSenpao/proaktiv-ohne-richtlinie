7f390408befd5958a7942170eafd72d992d9772e
TuniSenpao <robert-liefke@gmx.de>
2021-11-23 15:47:42 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -14,13 +14,13 @@ class ArztterminSkill(MycroftSkill):
     @intent_handler('Reminder.intent')
     def add_unspecified_reminder(self, msg=None):        
         # TIME:
-        time_response = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=-1)
+        time_response = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=20)
         # Check if a time was in the response
         dt, rest = extract_datetime(time_response) or (None, None)
         if dt or self.response_is_affirmative(time_response):
             if not dt:
                 # No time specified
-                time = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=-1)
+                time = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=20)
                 dt, rest = extract_datetime(time) or None, None
                 if not dt:
                     self.speak_dialog('Fine')
@@ -30,7 +30,7 @@ class ArztterminSkill(MycroftSkill):
 
         # DATE:
         date = None
-        date_response = self.get_response('ParticularDate', on_fail='wait.for.answer', num_retries=-1)
+        date_response = self.get_response('ParticularDate', on_fail='wait.for.answer', num_retries=20)
         months = ['januar', 'februar','märz', 'april', 'mai', 'juni', 'juli', 'august', 'september','oktober','november','dezember',
                 'januar.', 'februar.','märz.', 'april.', 'mai.', 'juni.', 'juli.', 'august.', 'september.','oktober.','november.','dezember.', '. erster', '. ersten', '. zweiter','. zweiten' ,'. dritter', '. dritten','. 3','. 4','. 5','. 6','. 7','. 8','. 9','. 10','. 11','. 12','. 13','. 14','. 15',
                 '. 16','. 17','. 18','. 19','. 20','. 21','. 22','. 23','. 24','. 25','. 26','. 27','. 28','. 29','. 30','. 31']
@@ -43,23 +43,23 @@ class ArztterminSkill(MycroftSkill):
             date = day[-1] + '. ' + month[-1]
         else:
             # TODO: besser nach wiederholung fragen
-            date_response = self.get_response('ParticularDateTwo', on_fail='wait.for.answer', num_retries=-1)
+            date_response = self.get_response('ParticularDateTwo', on_fail='wait.for.answer', num_retries=20)
             day = [d for d in days if(d in date_response)]
             month = [m for m in months if(m in date_response)]
             date = day[-1] + '. ' + month[-1]
 
         # NAME:
-        name = self.get_response('ParticularName', on_fail='wait.for.answer', num_retries=-1)
+        name = self.get_response('ParticularName', on_fail='wait.for.answer', num_retries=20)
         name = name.lower().replace('der', '').replace('termin', '').replace('ist', '').replace('bei', '').replace('er heißt', '').replace('ich', '').replace('glaube', '').replace('dieser', '').replace('mein', '').replace('arzttermin', '').replace('arzt', '').replace('ärztin', '')
 
         if (time is None):
-            time_response = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=-1)
+            time_response = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=20)
             # Check if a time was in the response
             dt, rest = extract_datetime(time_response) or (None, None)
             if dt or self.response_is_affirmative(time_response):
                 if not dt:
                     # No time specified
-                    time = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=-1)
+                    time = self.get_response('ParticularTime', on_fail='wait.for.answer', num_retries=20)
                     dt, rest = extract_datetime(time) or None, None
                     if not dt:
                         self.speak_dialog('Fine')
@@ -71,10 +71,10 @@ class ArztterminSkill(MycroftSkill):
 
             time = datetime.strftime(dt, "%H:%M")
         if (name is None):
-            name = self.get_response('ParticularName', on_fail='wait.for.answer', num_retries=-1)
+            name = self.get_response('ParticularName', on_fail='wait.for.answer', num_retries=20)
             name = name.lower().replace('der', '').replace('termin', '').replace('ist', '').replace('bei', '').replace('er heißt', '').replace('ich', '').replace('glaube', '').replace('dieser', '').replace('mein', '').replace('arzttermin', '').replace('arzt', '').replace('ärztin', '')
         if (date is None):
-            date_response = self.get_response('ParticularDate', on_fail='wait.for.answer', num_retries=-1)
+            date_response = self.get_response('ParticularDate', on_fail='wait.for.answer', num_retries=20)
             months = ['januar', 'februar','märz', 'april', 'mai', 'juni', 'juli', 'august', 'september','oktober','november','dezember']
             days = ['erster', 'ersten', 'zweiter','zweiten' ,'dritter', 'dritten','3','4','5','6','7','8','9','10','11','12','13','14','15',
                 '16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
